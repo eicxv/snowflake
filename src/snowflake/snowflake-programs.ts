754c7e1170cb2d0de7ffad70dac09decf8aa182f
Einar Persson <einar.jpg@gmail.com>
2022-04-07 15:03:56 +0200

    removed noise step as it not useful when using symmetry

--- src/snowflake/snowflake-programs.ts
+++ src/snowflake/snowflake-programs.ts
@@ -8,7 +8,6 @@ import dispSource from "./shaders/disp.frag?raw";
 import fillSource from "./shaders/fill.frag?raw";
 import freezingSource from "./shaders/freezing.frag?raw";
 import meltingSource from "./shaders/melting.frag?raw";
-import noiseSource from "./shaders/noise.frag?raw";
 import visualizationSource from "./shaders/visualization.frag?raw";
 import { SnowflakeUniformCollection } from "./snowflake-config";
 
@@ -287,47 +286,6 @@ export class MeltingProgram extends Program {
 
     gl.uniform1f(locations.u_mu, uniforms.u_mu);
     gl.uniform1f(locations.u_gamma, uniforms.u_gamma);
-
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, uniforms.u_latticeTexture);
-    gl.uniform1i(locations.u_latticeTexture, 0);
-
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 3);
-  }
-}
-
-export class NoiseProgram extends Program {
-  constructor(
-    gl: WebGL2RenderingContext,
-    uniforms: SnowflakeUniformCollection,
-    vao: WebGLVertexArrayObject,
-    framebuffer: WebGLFramebuffer | null = null
-  ) {
-    const fragShader = compileShader(gl, noiseSource, gl.FRAGMENT_SHADER);
-    const vertShader = compileShader(gl, computeSource, gl.VERTEX_SHADER);
-    const localUniforms = ["u_latticeTexture", "u_sigma", "u_step"];
-    super(
-      gl,
-      fragShader,
-      vertShader,
-      localUniforms,
-      uniforms,
-      vao,
-      framebuffer
-    );
-  }
-
-  run(): void {
-    const gl = this.gl;
-
-    gl.useProgram(this.program);
-    gl.bindFramebuffer(gl.FRAMEBUFFER, this.framebuffer);
-    gl.bindVertexArray(this.vao);
-    const locations = this.locations;
-    const uniforms = this.uniforms as SnowflakeUniformCollection;
-
-    gl.uniform1f(locations.u_sigma, uniforms.u_sigma);
-    gl.uniform1ui(locations.u_step, uniforms.u_step);
 
     gl.activeTexture(gl.TEXTURE0);
     gl.bindTexture(gl.TEXTURE_2D, uniforms.u_latticeTexture);
