57ba6306b4a7ad361afef4734b7f095989ee9a0a
Einar Persson <einar.jpg@gmail.com>
2022-04-14 04:30:11 +0200

    remove unused programs

--- src/snowflake/snowflake-render-programs.ts
+++ src/snowflake/snowflake-render-programs.ts
@@ -2,8 +2,6 @@ import { compileShader } from "../webgl/gl-utility";
 import { Program } from "../webgl/program";
 import computeFlippedSource from "./shaders/common/compute-flipped.vert?raw";
 import computeSource from "./shaders/common/compute.vert?raw";
-import dispSource from "./shaders/common/disp.frag?raw";
-import fillSource from "./shaders/common/fill.frag?raw";
 import displaySource from "./shaders/render/display.frag?raw";
 import interpolateSource from "./shaders/render/interpolate.frag?raw";
 import normalSource from "./shaders/render/normal.frag?raw";
@@ -178,85 +176,6 @@ export class NormalProgram extends Program {
     gl.bindVertexArray(this.vao);
     const locations = this.locations;
     const uniforms = this.uniforms as SnowflakeUniforms;
-
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, uniforms.u_latticeTexture);
-    gl.uniform1i(locations.u_latticeTexture, 0);
-
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 3);
-  }
-}
-
-export class DispProgram extends Program {
-  constructor(
-    gl: WebGL2RenderingContext,
-    uniforms: SnowflakeUniforms,
-    vao: WebGLVertexArrayObject,
-    framebuffer: WebGLFramebuffer | null = null
-  ) {
-    const fragShader = compileShader(gl, dispSource, gl.FRAGMENT_SHADER);
-    const vertShader = compileShader(gl, computeSource, gl.VERTEX_SHADER);
-    const localUniforms = ["u_latticeTexture"];
-    super(
-      gl,
-      fragShader,
-      vertShader,
-      localUniforms,
-      uniforms,
-      vao,
-      framebuffer
-    );
-  }
-
-  run(): void {
-    const gl = this.gl;
-
-    gl.useProgram(this.program);
-    gl.bindFramebuffer(gl.FRAMEBUFFER, this.framebuffer);
-    gl.bindVertexArray(this.vao);
-    const locations = this.locations;
-    const uniforms = this.uniforms as SnowflakeUniforms;
-
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, uniforms.u_latticeTexture);
-    gl.uniform1i(locations.u_latticeTexture, 0);
-
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
-  }
-}
-
-export class FillProgram extends Program {
-  constructor(
-    gl: WebGL2RenderingContext,
-    uniforms: SnowflakeUniforms,
-    vao: WebGLVertexArrayObject,
-    framebuffer: WebGLFramebuffer | null = null
-  ) {
-    const fragShader = compileShader(gl, fillSource, gl.FRAGMENT_SHADER);
-    const vertShader = compileShader(gl, computeSource, gl.VERTEX_SHADER);
-    const localUniforms = ["u_latticeTexture", "u_rho"];
-    super(
-      gl,
-      fragShader,
-      vertShader,
-      localUniforms,
-      uniforms,
-      vao,
-      framebuffer
-    );
-  }
-
-  run(): void {
-    const gl = this.gl;
-
-    gl.useProgram(this.program);
-    gl.bindFramebuffer(gl.FRAMEBUFFER, this.framebuffer);
-    gl.bindVertexArray(this.vao);
-
-    const locations = this.locations;
-    const uniforms = this.uniforms as SnowflakeUniforms;
-
-    gl.uniform1f(locations.u_rho, uniforms.u_rho);
 
     gl.activeTexture(gl.TEXTURE0);
     gl.bindTexture(gl.TEXTURE_2D, uniforms.u_latticeTexture);
